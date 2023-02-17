849226958c277183d111ec96a25a2688a4933d87
Ilyes Braham <ilyes.braham@live.fr>
2023-02-17 15:10:44 +0100

    feat: add mypy to hooks on pre-commit configs

--- example.py
+++ example.py
@@ -8,3 +8,12 @@ import mylib  # noqa (i.e ignore cette erreur)
 
 def foo(bar):
     print("foo {}".format(bar))
+
+
+def ratio_example_for_mypy(numerator, denominator):
+    result = numerator / denominator
+    return result
+
+
+ratio_1 = ratio_example_for_mypy(6, 2)
+print(ratio_1)
